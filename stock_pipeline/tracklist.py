71db3c6910bb52d20a75adafca043e3e2ea91aa7
Abhijit Kumar Singh <thisabhijithere@gmail.com>
2025-08-10 09:02:26 +0000

    Added tracklist

---
+++ stock_pipeline/tracklist.py
@@ -0,0 +1,8 @@
+"""Symbols to track.
+
+Edit this file and change the `track` list to control which symbols are refreshed.
+A change to this file will trigger the Dagster sensor to run the asset automatically.
+"""
+
+# Example symbols
+track = ["AMZN"]
